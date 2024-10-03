e1e64a90bcf29c694cefa54f19e74283ad8c59d2
Hyebin Lee <112963362+Hbyien@users.noreply.github.com>
2024-10-03 17:37:23 +0900

    Drill 06 제출 (2020180050 이혜빈)

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -20,7 +20,6 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
